ea842ad88cdb40611a085802968b793fc4ed7e28
MinhmpcNguyen <ntm.cmv@gmail.com>
2024-05-14 09:14:39 +0700

    changed files in assignment folder

---
+++ GUIProject/src/hust/soict/dsai/aims/screen/manager/StoreManagerScreen.java
@@ -0,0 +1,183 @@
+package hust.soict.dsai.aims.screen.manager;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
+public class StoreManagerScreen extends JFrame {
+    private Store store;
+
+    public StoreManagerScreen(Store store) {
+        this.store = store;
+        
+        Container cp = getContentPane();
+        cp.setLayout(new BorderLayout());
+        cp.add(createNorth(), BorderLayout.NORTH);
+        cp.add(createCenter(), BorderLayout.CENTER);
+        
+        setTitle("Store");
+        setSize(1024, 768);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    JPanel createNorth() {
+        JPanel north = new JPanel();
+        north.setLayout(new BoxLayout(north, BoxLayout.Y_AXIS));
+        north.add(createMenuBar());
+        north.add(createHeader());
+        return north;
+    }
+
+    JMenuBar createMenuBar() {
+        JMenu menu = new JMenu("Options");
+        menu.add(new JMenuItem("View store"));
+
+        JMenu smUpdateStore = new JMenu("Update Store");
+        smUpdateStore.add(new JMenuItem("Add Book"));
+        smUpdateStore.add(new JMenuItem("Add CD"));
+        smUpdateStore.add(new JMenuItem("Add DVD"));
+        menu.add(smUpdateStore);
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setLayout(new FlowLayout(FlowLayout.LEFT));
+        menuBar.add(menu);
+
+        return menuBar;
+    }
+
+    JPanel createHeader() {
+        JPanel header = new JPanel();
+        header.setLayout(new BoxLayout(header, BoxLayout.X_AXIS));
+
+        JLabel title = new JLabel("AIMS");
+        title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 50));
+        title.setForeground(Color.CYAN);
+
+        header.add(Box.createRigidArea(new Dimension(10, 10)));
+        header.add(title);
+        header.add(Box.createHorizontalGlue());
+        header.add(Box.createRigidArea(new Dimension(10, 10)));
+
+        return header;
+    }
+
+    JPanel createCenter() {
+        JPanel center = new JPanel();
+        center.setLayout(new GridLayout(3, 3, 2, 2));
+
+        ArrayList<Media> mediaInStore = store.getItemsInStore();
+        for (int i = 0; i < mediaInStore.size() && i < 9; i++) {
+            MediaStore cell = new MediaStore(mediaInStore.get(i));
+            center.add(cell);
+        }
+
+        return center;
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                Store store = new Store();
+                new StoreManagerScreen(store);
+            }
+        });
+    }
+}
+
+class Store {
+    private ArrayList<Media> itemsInStore = new ArrayList<>();
+
+    public Store() {
+        // Add some sample media
+        itemsInStore.add(new Book("Book 1", 10.0));
+        itemsInStore.add(new CD("CD 1", 15.0));
+        itemsInStore.add(new DVD("DVD 1", 20.0));
+    }
+
+    public ArrayList<Media> getItemsInStore() {
+        return itemsInStore;
+    }
+}
+
+class Media {
+    private String title;
+    private double cost;
+
+    public Media(String title, double cost) {
+        this.title = title;
+        this.cost = cost;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+}
+
+interface Playable {
+    void play();
+}
+
+class Book extends Media {
+    public Book(String title, double cost) {
+        super(title, cost);
+    }
+}
+
+class CD extends Media implements Playable {
+    public CD(String title, double cost) {
+        super(title, cost);
+    }
+
+    @Override
+    public void play() {
+        // Play CD
+    }
+}
+
+class DVD extends Media implements Playable {
+    public DVD(String title, double cost) {
+        super(title, cost);
+    }
+
+    @Override
+    public void play() {
+        // Play DVD
+    }
+}
+
+class MediaStore extends JPanel {
+    private Media media;
+
+    public MediaStore(Media media) {
+        this.media = media;
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+        JLabel title = new JLabel(media.getTitle());
+        title.setFont(new Font(title.getFont().getName(), Font.PLAIN, 15));
+        title.setAlignmentX(CENTER_ALIGNMENT);
+
+        JLabel cost = new JLabel(media.getCost() + " $");
+        cost.setAlignmentX(CENTER_ALIGNMENT);
+
+        JPanel container = new JPanel();
+        container.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        if (media instanceof Playable) {
+            JButton playButton = new JButton("Play");
+            container.add(playButton);
+        }
+
+        this.add(Box.createVerticalGlue());
+        this.add(title);
+        this.add(cost);
+        this.add(Box.createVerticalGlue());
+        this.add(container);
+
+        this.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+    }
+}
